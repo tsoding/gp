fe5d6ab2b98ecc8ffd6071f91f8fdc887e3663de
rexim <reximkut@gmail.com>
2020-11-13 20:52:57 +0700

    Put gnuplot functionality of gp_trainer under a flag

--- src/gp_trainer.c
+++ src/gp_trainer.c
@@ -25,7 +25,11 @@ const char *shift(int *argc, char ***argv)
 
 void usage(FILE *stream)
 {
-    fprintf(stream, "Usage: ./gp_trainer <generations-count> <output.bin>\n");
+    fprintf(stream, "Usage: ./gp_trainer [OPTIONS]\n");
+    fprintf(stream, "    -g <NUMBER>    Amount of generations (default: 69)\n");
+    fprintf(stream, "    -o <FILEPATH>  Output file path (default: output.bin)\n");
+    fprintf(stream, "    -gp            Enable epic gnuplot reporting (requires gnuplot, duh)\n");
+    fprintf(stream, "    -h             Print this help\n");
 }
 
 int main(int argc, char *argv[])
@@ -37,22 +41,49 @@ int main(int argc, char *argv[])
     // - Continue training from a provided dumped state
     // - Maybe log each generations during the training
 
-    if (argc == 0) {
-        usage(stderr);
-        fprintf(stderr, "ERROR: number of generations is not provided\n");
-        exit(1);
-    }
-    int generations_count = atoi(shift(&argc, &argv));
+    int generations_count = 69;
+    const char *output_filepath = "output.bin";
+    int gnuplot = 0;
 
-    if (argc == 0) {
-        usage(stderr);
-        fprintf(stderr, "ERROR: output filepath is not provided\n");
-        exit(1);
-    }
-    const char *output_filepath = shift(&argc, &argv);
+    while (argc > 0) {
+        const char *flag = shift(&argc, &argv);
 
-    FILE* log = fopen("log.dat", "w");
-    log_header(log);
+        if (strcmp(flag, "-g") == 0) {
+            if (argc == 0) {
+                usage(stderr);
+                fprintf(stderr, "ERROR: no value provided for flag `%s`\n", flag);
+                exit(1);
+            }
+
+            const char *value = shift(&argc, &argv);
+            generations_count = atoi(value);
+        } else if (strcmp(flag, "-o") == 0) {
+            if (argc == 0) {
+                usage(stderr);
+                fprintf(stderr, "ERROR: no value provided for flag `%s`\n", flag);
+                exit(1);
+            }
+
+            const char *value = shift(&argc, &argv);
+            output_filepath = value;
+        } else if (strcmp(flag, "-gp") == 0) {
+            gnuplot = 1;
+        } else if (strcmp(flag, "-h") == 0) {
+            usage(stdout);
+            exit(0);
+        } else {
+            usage(stderr);
+            fprintf(stderr, "ERROR: unknown flag `%s`\n", flag);
+            exit(1);
+        }
+    }
+
+    FILE* log = NULL;
+
+    if (gnuplot) {
+        log = fopen("log.dat", "w");
+        log_header(log);
+    }
 
     int current = 0;
     init_game(&games[current]);
@@ -67,18 +98,22 @@ int main(int argc, char *argv[])
         printf("%fs\n", (float)(clock() - begin) / (float) CLOCKS_PER_SEC);
         fflush(stdout);
 
-        log_generation(log, i, &games[current]);
-        /* we need to fflush the log file in order to use the live update */
-        fflush(log);
-        log_live_update();
+        if (gnuplot) {
+            log_generation(log, i, &games[current]);
+            /* we need to fflush the log file in order to use the live update */
+            fflush(log);
+            log_live_update();
+        }
 
         int next = 1 - current;
         make_next_generation(&games[current], &games[next]);
         current = next;
     }
 
-    log_close_pipe();
-    fclose(log);
+    if (gnuplot) {
+        log_close_pipe();
+        fclose(log);
+    }
 
     dump_game(output_filepath, &games[current]);
 
